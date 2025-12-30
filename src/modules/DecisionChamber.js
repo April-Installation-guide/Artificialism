b35e39f013c54be4bbf4c952c1ba80bc699a6b3f
April-Installation-guide <sauljalinas1@gmail.com>
2025-12-30 21:30:16 +0100

    Create DecisionChamber.js

---
+++ src/modules/DecisionChamber.js
@@ -0,0 +1,31 @@
+// src/modules/DecisionChamber.js
+
+class DecisionChamber {
+    constructor() {
+        this.chamberName = 'DecisionChamber';
+        this.chamberVersion = '1.0.0';
+        this.decisionLog = [];
+        
+        // Estrategias de decisión
+        this.decisionStrategies = {
+            // ... (tu action selector original adaptado)
+        };
+    }
+
+    /**
+     * Tomar decisión basada en análisis del concilio
+     */
+    async makeCouncilDecision(question, analyses, knowledge, context) {
+        // Integrar decisiones basadas en:
+        // 1. Análisis psicológico
+        // 2. Análisis filosófico  
+        // 3. Conocimiento integrado
+        // 4. Contexto del usuario
+        
+        // ... (lógica de decisión mejorada)
+    }
+
+    // ... (métodos de decisión)
+}
+
+export default DecisionChamber;
